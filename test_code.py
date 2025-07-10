671577db178b69b5faf407488ee36b6060a7a363
Clovis G. Carmo <122237117+ClovisCarmo@users.noreply.github.com>
2025-07-10 11:37:42 -0300

    Update test_code.py

--- test_code.py
+++ test_code.py
@@ -2,9 +2,14 @@ import algorithms
 import data_creation.run_data
 import graphstk
 import time
+import pandas as pd
+
+df = pd.read_csv("dataset-grafos-trab-final.csv")
+
+dataset = df['graph']
 
 start = time.time()
-run_dataset(dataset_total, modified_kruskal)
+run_dataset(dataset, modified_kruskal)
 end = time.time()
 
 print(f"Tempo de execução: {end - start} segundos")
